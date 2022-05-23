4769bb428ad492a99cf950c42faddac54c29ad96
Dwise09 <101394519+Dwise09@users.noreply.github.com>
2022-05-23 22:36:44 +0100

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ *  _printf - prints %c and %s format specifier
+ *  @format: format string
+ *  Return: number of printed charaters
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int a = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(valist, format);
+
+	while (format[a])
+	{
+		for (; format[a] != '%' && format[a]; a++)
+		{
+			_putchar(format[a]);
+			count++;
+		}
+
+		if (!format[a])
+			return (count);
+
+		f = check_specifier(&format[a + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			a += 2;
+			continue;
+		}
+		if (!format[a + 1])
+			return (-1);
+		_putchar(format[a]);
+		count++;
+		if (format[a + 1] == '%')
+			a += 2;
+		else
+			a++;
+	}
+	va_end(valist);
+	return (count);
+}
